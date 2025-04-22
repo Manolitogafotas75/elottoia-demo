818458c503ec7d923992fa85f95d64b14540d259
Manolitogafotas75 <unmagma@gmail.com>
2025-04-22 18:44:12 +0200

    elottoia_demo_app.py

--- elottoia_demo_app.py
+++ elottoia_demo_app.py
@@ -9,7 +9,7 @@ st.markdown("Bienvenido a la versión de prueba de ElottoIA.")
 st.markdown("**Solo está disponible el modo de generación Aleatorio.**")
 st.markdown("")
 
-st.markdown("> 🧪 **Si quieres disfrutar de todas las ventajas que ofrece nuestra versión extendida y mejorada, prueba la versión web completa justo debajo (botón verde) (https://elottoia-landing.onrender.com//)**")
+st.markdown("> 🧪 **Si quieres disfrutar de todas las ventajas que ofrece nuestra versión extendida y mejorada, prueba la versión web completa por tan solo 4,99 €.Encontrarás el enlace de compra justo debajo,en este enlace (https://elottoia-landing.onrender.com//)**")
 
 # Idioma
 lang = st.selectbox("🌐 Idioma / Language", ["Español", "English"])
